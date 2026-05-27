4046b0754d087cc9b0a73917de7f7585589a67f2
who-shubham <shubham.2022ug2012@iiitranchi.ac.in>
2026-05-28 02:48:58 +0530

    add seach suggestions

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const result = document.getElementById("result")
 const cityInput = document.getElementById("city")
 const suggestions = document.getElementById("suggestions");
 
+const apiKey = "0ee3c341986c4c7e4f2ad97a4dcb8719"
 
 const handler = async () => {
-    const apiKey = "0ee3c341986c4c7e4f2ad97a4dcb8719"
     const city = cityInput.value
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
@@ -20,7 +20,7 @@ const handler = async () => {
 
     result.innerHTML = `
     <h2>City: ${data.name}</h2>
-    <p>Temp: ${data.main.temp}</p>
+    <p>Temp: ${Math.round(data.main.temp)}</p>
     <p>Weather: ${data.weather[0].main}</p>
     `
 }
@@ -31,17 +31,31 @@ cityInput.addEventListener("keypress", (e) => {
     if (e.key == "Enter") {
         handler()
         cityInput.value = ""
+        suggestions.innerHTML = ""
     }
 })
 
-cityInput.addEventListener("input", async () {
+cityInput.addEventListener("input", async () => {
     const city = cityInput.value.trim()
     const geourl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
 
-    const res = await geourl
+    const res = await fetch(geourl)
     const data = await res.json()
 
     suggestions.innerHTML = ""
 
-    
+    data.forEach(place => {
+        const div = document.createElement("div")
+        div.className = "suggestion-item"
+
+        div.innerText = `${place.name}, ${place.state || ""}, ${place.country}`
+
+        div.addEventListener("click", () => {
+            cityInput.value = place.name
+            suggestions.innerHTML = ""
+            handler()
+        })
+
+        suggestions.appendChild(div)
+    });
 })
